1ad88dd97e0833c28d00bceb4243cf6728fe0650
missionfloyd <missionfloyd@users.noreply.github.com>
2021-05-29 19:21:37 -0600

    Update random.py

--- hitherdither/random.py
+++ hitherdither/random.py
@@ -1,10 +1,5 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""
-bayer_dithering
------------
-
-"""
 
 from __future__ import division
 from __future__ import print_function
@@ -12,9 +7,8 @@ from __future__ import unicode_literals
 from __future__ import absolute_import
 
 import numpy as np
-import math
 
-def random_noise_dithering(image, palette, order=64):
+def random_noise_dithering(image, palette, order=128):
     """Render the image using the random noise dithering pattern.
 
     Reference: http://caca.zoy.org/wiki/libcaca/study/1
@@ -23,37 +17,33 @@ def random_noise_dithering(image, palette, order=64):
     :param :class:`PIL.Image` image: The image to apply the
         ordered dithering to.
     :param :class:`~hitherdither.colour.Palette` palette: The palette to use.
-    :param int order: Standard deviation (spread or “width”) of the
-        distribution. Must be non-negative.
-    :return:  The random noise dithered PIL image of type "P"
-        using the input palette.
+    :param int order: Noise intensity
+    :return:  The dithered PIL image of type "P" using the input palette.
 
     """
 
-    ni = np.array(image, "uint8")
+    ni = np.array(image)
     noise = np.random.normal(0, order, ni.shape)
     new_image = ni + noise
     return palette.create_PIL_png_from_rgb_array(new_image)
-   
 
 def interleaved_gradient_noise(image, palette, order=128):
     """Render the image using the interleaved gradient noise pattern.
 
     Reference:
-        https://bartwronski.com/2016/10/30/dithering-part-three-real-world-2d-quantization-dithering/
+        http://www.iryoku.com/next-generation-post-processing-in-call-of-duty-advanced-warfare
 
     :param :class:`PIL.Image` image: The image to apply the
         ordered dithering to.
     :param :class:`~hitherdither.colour.Palette` palette: The palette to use.
-    :param int order:
-    :return:  The random noise dithered PIL image of type "P"
-        using the input palette.
+    :param int order: Noise intensity
+    :return: The dithered PIL image of type "P" using the input palette.
 
     """
 
-    ni = np.array(image, "uint8")
+    ni = np.array(image)
     noise = np.zeros(ni.shape[:2], "float")
     for x, y in np.ndindex(noise.shape):
-        noise[x, y] = math.modf(52.9829189 * math.modf(0.06711056 * x + 0.00583715 * y)[0])[0] * 255 - order
+        noise[x, y] = (((52.9829189 * (0.06711056 * x + 0.00583715 * y) % 1) % 1) - 0.5) * order
     new_image = ni + np.repeat(np.expand_dims(noise, 2), 3, 2)
     return palette.create_PIL_png_from_rgb_array(new_image)
